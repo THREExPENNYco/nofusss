ded7dae21835ed092cbeb35ddca2b094cd38a834
Devoun Edwards <THREExPENNYco@gmail.com>
2020-02-05 01:28:30 -0700

    erasing uncessary import on form function

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { startSession, STATES } from "mongoose";
 
 function Form() {
   const [firstName, setFirstName] = useState("");

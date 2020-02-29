22a10ad66e2587993c770b8d1a4c66a4d15c0a7f
Devoun Edwards <THREExPENNYco@gmail.com>
2020-02-28 21:07:46 -0700

    configuration changes for heroku

--- backend/models/potenCustModel.js
+++ backend/models/potenCustModel.js
@@ -1,76 +1,78 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+/* eslint-disable linebreak-style */
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 
 const potenCust = new Schema(
   {
     firstname: {
       type: String,
-      required: [true, "A first name is required"],
+      required: [true, 'A first name is required'],
       min: 2,
       max: 40,
-      trim: true
+      trim: true,
     },
     lastname: {
       type: String,
-      required: [true, "A last name is required"],
+      required: [true, 'A last name is required'],
       min: 2,
       max: 40,
-      trim: true
+      trim: true,
     },
     email: {
       type: String,
-      required: [true, "An email address is required"],
+      required: [true, 'An email address is required'],
       trim: true,
       validate: {
-        validator: function(email) {
+        validator(email) {
           return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-            email
+            email,
           );
         },
-        message: "Please use a valid email address"
-      }
+        message: 'Please use a valid email address',
+      },
     },
     message: {
       type: String,
-      min: [10, "Field cannot be empty"],
+      min: [10, 'Field cannot be empty'],
       max: 200,
-      required: [true, "We really wanna know how we can help."]
+      required: [true, 'We really wanna know how we can help.'],
     },
-    location: { 
-      type: String, 
-      required: [true, "We currently only service some locations."], 
-      enum: ['Houston', 'San Antonio', 'Austin']
-    }, 
-    currsupplier: { 
-      type: String, 
-      min: [10, "Field cannot be empty"], 
-      max: 20, 
-      required: [true, "This is required to personalize your quote"]
-    }, 
+    location: {
+      type: String,
+      required: [true, 'We currently only service some locations.'],
+      enum: ['Houston', 'San Antonio', 'Austin'],
+    },
+    currsupplier: {
+      type: String,
+      min: [10, 'Field cannot be empty'],
+      max: 20,
+      required: [true, 'This is required to personalize your quote'],
+    },
     company: {
-      type: String, 
+      type: String,
       min: 2,
       max: 40,
       trim: true,
-      required: [true, "A company is required"]
+      required: [true, 'A company is required'],
     },
     phonenumber: {
       type: String,
       trim: true,
-      required: [true, "A phone number is required."],
+      required: [true, 'A phone number is required.'],
       validate: {
-        validator: function(number) {
+        validator(number) {
           return /\d{3}-\d{3}-\d{4}/.test(number);
         },
-        message: "Please use a valid phone number"
-      }
-    }
+        message: 'Please use a valid phone number',
+      },
+    },
   },
   {
-    timestamps: true
-  }
+    timestamps: true,
+  },
 );
 
-const PotenCust = mongoose.model("PotenCust", potenCust);
+const PotenCust = mongoose.model('PotenCust', potenCust);
 
 module.exports = PotenCust;

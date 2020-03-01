7b667c7278aebb539934bccf3072f0c9607688ce
Devoun Edwards <THREExPENNYco@gmail.com>
2020-03-01 10:46:43 -0700

    refactoring css

--- src/components/snippet.jsx
+++ src/components/snippet.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import chemicalpng from "../components/componentAssets/chemicalpng.png";
-import familyownedpng from "../components/componentAssets/familyownedpng.png";
-import paperpng from "../components/componentAssets/paperpng.png";
-import { Spring } from "react-spring/renderprops";
+import React from 'react';
+import { Spring } from 'react-spring/renderprops';
+import chemicalpng from './componentAssets/chemicalpng.png';
+import familyownedpng from './componentAssets/familyownedpng.png';
+import paperpng from './componentAssets/paperpng.png';
 
 function Snippet(props) {
-  if (props.image === "chemicalpng") {
+  if (props.image === 'chemicalpng') {
     var image = chemicalpng;
-  } else if (props.image === "familyownedpng") {
+  } else if (props.image === 'familyownedpng') {
     var image = familyownedpng;
-  } else if (props.image === "paperpng") {
+  } else if (props.image === 'paperpng') {
     var image = paperpng;
   }
 
@@ -19,11 +19,11 @@ function Snippet(props) {
       to={{ opacity: 1 }}
       config={{ duration: 1000 }}
     >
-      {spring => (
+      {(spring) => (
         <article style={spring}>
-          <img className="snippet-images" src={image}></img>
+          <img className="snippet-images" src={image} />
           <h1 className="article-title">{props.snippetHead}</h1>
-          <hr></hr>
+          <hr />
           <p className="article-par">{props.snippet}</p>
         </article>
       )}
